9d3c5690517d5ea31648fe1c589cf805ecc26819
derrick fink <derrickfink@boisestate.edu>
2023-01-20 21:24:21 -0700

    init client

--- client/devToolbox/src/app/app.component.ts
+++ client/devToolbox/src/app/app.component.ts
@@ -1,44 +1,19 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
+import { Component } from '@angular/core';
 @Component({
-  templateUrl: 'app.html'
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
-export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-
-  rootPage: any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
-
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
-
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+export class AppComponent {
+  public appPages = [
+    { title: 'Todo List', url: '/folder/Inbox', icon: 'list' },
+    { title: 'Pomodoro Timer', url: '/folder/Outbox', icon: 'timer' },
+    { title: 'JWT Decoder', url: '/folder/Favorites', icon: 'barcode' },
+    { title: 'Img Formatter', url: '/folder/Archived', icon: 'cloud-upload' },
+    { title: 'Scratchpad', url: '/folder/Trash', icon: 'pencil' },
+    { title: 'Kanban Board', url: '/folder/Spam', icon: 'apps' },
+    { title: 'Kanban Board', url: '/folder/Spam', icon: 'apps' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
 }
